3067bcd0bd6a772441b34f21caabe47266d739eb
Isaac Weaver <wisaac407@gmail.com>
2019-07-02 10:53:16 -0500

    Version patch

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ from . import addon_updater_ops
 
 bl_info = {
     "name": "iMeshh Asset Manager",
-    "version": (0, 2, 0),
+    "version": (0, 2, 1),
     "blender": (2, 80, 0),
     "location": "View3D > TOOLS > iMeshh",
     "author": "iMeshh",

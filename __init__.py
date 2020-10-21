53ff7ccb39ed2476fc286973bf3fbcfaeed0995b
studiomonkriss <69104667+studiomonkriss@users.noreply.github.com>
2020-10-21 18:41:49 +0200

    Revert "Version Bump 2.56"
    This reverts commit e4ad3d5cbda41ae8c911b8356ec3550b9bbe6fc5.

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ from . import addon_updater_ops
 
 bl_info = {
     "name": "iMeshh Asset Manager",
-    "version": (0, 2, 56),
+    "version": (0, 2, 55),
     "blender": (2, 80, 0),
     "location": "View3D > TOOLS > iMeshh",
     "author": "iMeshh",
